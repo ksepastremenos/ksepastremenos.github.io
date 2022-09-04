d8a329e5580ae487dca5a974ed7b9a9147e9db2e
ksepastremenos <rex_kungolos@hotmail.com>
2022-09-04 13:31:00 +0300

    massive update

--- myscript.js
+++ myscript.js
@@ -135,3 +135,18 @@ function swapContent(newcontent){
 
 }
 
+//collapsable menus
+let coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activeCollapsible");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
